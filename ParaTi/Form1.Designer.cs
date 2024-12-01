7d5ee050ad94584009c871a54c752634b8d5393e
jadg13 <86695996+jadg-13@users.noreply.github.com>
2024-11-30 21:01:54 -0600

    Agregar archivos de proyecto.

---
+++ ParaTi/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ParaTi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(705, 525);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 28);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(767, 580);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ ParaTi/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParaTi
+{
+    public partial class Form1 : Form
+    {
+        // Lista para almacenar las rutas de las imágenes
+        private List<string> imagePaths;
+        // Índice actual de la imagen mostrada
+        private int currentImageIndex;
+
+        // Variables para controlar el movimiento del formulario
+        private bool isDragging = false;
+        private Point dragStartPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeImagePaths();
+            // Configurar el formulario
+            this.FormBorderStyle = FormBorderStyle.None; // Sin bordes
+            this.BackColor = Color.Black; // Color de fondo que se volverá transparente
+            this.TransparencyKey = Color.Black; // El mismo color que BackColor será transparente
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+        }
+
+        private void InitializeImagePaths()
+        {
+            // Inicializa la lista de rutas de las imágenes
+            imagePaths = new List<string>();
+            string imagesFolder = Path.Combine(Application.StartupPath, "Images");
+
+            // Verifica si la carpeta existe
+            if (Directory.Exists(imagesFolder))
+            {
+                // Obtiene todas las rutas de las imágenes en la carpeta
+                string[] files = Directory.GetFiles(imagesFolder, "*.png"); // Puedes cambiar el filtro si usas otro formato
+                imagePaths.AddRange(files);
+            }
+
+            // Inicializa el índice de la imagen actual
+            currentImageIndex = 0;
+
+            // Muestra la primera imagen si hay alguna
+            if (imagePaths.Count > 0)
+            {
+                pictureBox1.Image = Image.FromFile(imagePaths[currentImageIndex]);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                dragStartPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = false;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point difference = new Point(e.X - dragStartPoint.X, e.Y - dragStartPoint.Y);
+                this.Location = new Point(this.Location.X + difference.X, this.Location.Y + difference.Y);
+            }
+        }
+
+        private void crecerAlto()
+        {
+            if (Height < 580) Height += 10;
+
+        }
+
+        private void crecerAncho()
+        {
+            if (Width < 767)
+                Width += 10;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            crecerAlto();
+            crecerAncho();
+            florAleatoria();
+            if (Opacity < 1)
+                Opacity += 0.01;
+            else
+                timer1.Stop();
+
+        }
+
+        private void florAleatoria()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(imagePaths.Count);
+            pictureBox1.Image = Image.FromFile(imagePaths[index]);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Height = 0;
+            Width = 0;
+            Opacity = 0;
+            timer1.Interval = 100;
+            timer1.Tick += timer1_Tick;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            florAleatoria();    
+        }
+    }
+}
